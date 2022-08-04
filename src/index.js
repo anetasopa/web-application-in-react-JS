b93ec16096ecce61afdb5394c48dcc60ad155b14
Bombel <bombel@bombel.com>
2022-08-04 19:21:57 +0200

    Create more products

---
+++ src/index.js
@@ -0,0 +1,18 @@
+runApp();
+
+function runApp() {
+  
+  const product1 = new Product("Laptop", "eletronics", 500, "usd", "some_url_of_image");
+  const product2 = new Product("Super shoes", "clothing", 47, "usd", "some_url_of_image");
+  const product3 = new Product("Smartphone", "phones", 50, "usd", "some_url_of_image");
+  const product4 = new Product("Fancy Hat", "clothing", 100, "usd", "some_url_of_image");
+
+  const products = [product1, product2, product3, product4];
+
+  const container = document.getElementById("productContainer");
+
+  products.forEach(product => {
+    const productElement = createProductElement(product);
+    container.appendChild(productElement);
+  })  
+}
