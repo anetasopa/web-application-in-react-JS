4df1c1c10b6586db0f80b306c69215e1fbcbb131
Bombel <bombel@bombel.com>
2022-08-07 12:45:09 +0200

    Promise

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
 runApp();
 
-// async function runApp() {
-//   const responce = await getProductDataAsync();
-//   const productData = await responce.json();
-
-//     const products = productData.map(item => {
-//       return new Product(item.name, item.type, item.price, item.currency, item.image);
-//     })
-  
-//     const container = document.getElementById("productContainer");
-  
-//     products.forEach(product => {
-//       const productElement = createProductElement(product);
-//       container.appendChild(productElement);
-//     })
-
-//   console.log(productData)
-// }
-
 async function runApp() {
-  getProductDataAsync().then((responce) => {
-    responce.json().then((productData) => {
+  try {
+    const responce = await getProductDataAsync();
+    const productData = await responce.json();
+  
       const products = productData.map(item => {
         return new Product(item.name, item.type, item.price, item.currency, item.image);
       })
-
+    
       const container = document.getElementById("productContainer");
-
+    
       products.forEach(product => {
         const productElement = createProductElement(product);
         container.appendChild(productElement);
       })
-    })
-  }).catch((error) => {
-    console.log(error.message)
-  })
+  } catch(error) {
+    console.error(error.message);
+  }
+  
 }
+
+// async function runApp() {
+//   getProductDataAsync()
+//   .then((responce) => {
+//     responce.json().then((productData) => {
+//       const products = productData.map(item => {
+//         return new Product(item.name, item.type, item.price, item.currency, item.image);
+//       })
+
+//       const container = document.getElementById("productContainer");
+
+//       products.forEach(product => {
+//         const productElement = createProductElement(product);
+//         container.appendChild(productElement);
+//       })
+//     })
+
+//   }).catch((error) => {
+//     console.log(error.message)
+//   })
+// }
 
